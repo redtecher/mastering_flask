e327787861ca66b70c1797d78f280230974d7465
Hong Chuan <hongchuan95@live.com>
2018-07-07 09:48:19 +0800

    July 7th Update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 from flask_script import Manager,Server
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate,MigrateCommand
-
+from sqlalchemy import func
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
@@ -13,14 +14,52 @@ tags = db.Table('post_tags',
     db.Column('tag_id',db.Integer,db.ForeignKey('tag.id'))
     )
 
+    
+@app.route('/')
+@app.route('/<int:page>')
+def home(page=1):
+    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page,10)
+    recent,top_tags = sidebar_data()
+    return render_template('home.html',posts= posts,rencent=recent,top_tags=top_tags)
 
+
+def sidebar_data():
+    recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
+    top_tags =db.session.query(Tag,func.count(tags.c.post_id).label('total')).join(tags).group_by(Tag).order_by('total DESC').limit(5).all()
+    return recent,top_tags
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    tags = post.tags
+    recent,top_tags = sidebar_data()
+    comments = post.comments.order_by(Comment.date.desc()).all()
+    return render_template('post.html',post=post,tags=tags,recent=recent,top_tags=top_tags,comments=comments)
+
+
+
+@app.route('/tag/<string:tag_name>')
+def tag(tag_name):
+    tag = Tag.query.filter_by(title=tag_name).first_or_404()
+    posts = tag.posts.order_by(Post.publish_date.desc()).all()
+    recent,top_tags = sidebar_data()
+
+    return render_template('tag.html',tag= tag,posts = posts,recent=recent,top_tags=top_tags)
+
+@app.route('/user/<string:username>')
+def user(username):
+    user =User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.publish_date.desc()).all()
+    recent,top_tags =sidebar_data()
+
+    return render_template('user.html',user=user,posts=posts,recent=recent,top_tags=top_tags)
 
 
 class User(db.Model):
     id =db.Column(db.Integer(),primary_key = True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
- 
     posts = db.relationship('Post',backref = 'user',lazy = 'dynamic')
     def __init__(self, username):
         self.username=username
@@ -36,7 +75,6 @@ class Post(db.Model):
     publish_date = db.Column(db.DateTime())
     user_id = db.Column(db.Integer(),db.ForeignKey('user.id'))
     comments = db.relationship('Comment',backref = 'post',lazy = 'dynamic')
-
     tags=db.relationship(
         'Tag',secondary =tags,backref = db.backref('posts',lazy='dynamic')
     )
--- manage.py
+++ manage.py
@@ -1,19 +1,19 @@
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 
-from main import app,db,User
-
+from main import app,db,User,Post,Tag
+migrate = Migrate(app,db)
 manager = Manager(app)
 manager.add_command("runserver",Server())
+manager.add_command('db',MigrateCommand)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User,Post = Post,Tag=Tag)
 
 
-@app.route('/')
-def home():
-    return '<h1>Hello World</h1>'
+
 
 if __name__=='__main__':
     manager.run()
